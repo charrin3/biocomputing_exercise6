3774ad7a80875d02459663c6e05b76f57eea37c5
Caitlin Harrington <cnharrington97@gmail.com>
2018-10-10 19:57:32 -0400

    assignment 6 final

--- assignment6.py
+++ assignment6.py
@@ -7,11 +7,19 @@ Created on Mon Oct  8 21:47:25 2018
 """
 
 #code that replicates the functionality of head
-import pandas
-wages=pandas.read_csv("wages.csv")
-head10=wages.head(n=10)
+def head(x,z):
+    filedesired=open(x)
+    linenumber=0
+    for l in filedesired: 
+        linenumber=linenumber+1
+        if linenumber > z:
+            break
+        print l
+        
+head ("wages.csv",4)
 
 #iris.csv file
+import pandas
 iris=pandas.read_csv("iris.csv")
 
 #print the last 2 rows and the last two columns in the terminal
@@ -20,9 +28,11 @@ print(row_column)
 
 #get the number of observations for each species in the set
 observations=iris['Species'].value_counts()
+print(observations)
 
 #get rows with sepal.width > 3.5
 sepalwidth=iris[iris['Sepal.Width']>3.5]
+print(sepalwidth)
 
 #write the data for the species setosa to a column delimited file
 setosadf=iris[iris['Species']=="setosa"]
